2c706f4230ef3a8c251e8fbd58ac801e81d3ae40
nhatminhdao <nhatminhdao@gmail.com>
2024-08-07 19:40:28 +0000

    commit

--- chat.rb
+++ chat.rb
@@ -1,7 +1,24 @@
 require "openai"
 
-# pp "howdy"
-
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
-pp client
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
